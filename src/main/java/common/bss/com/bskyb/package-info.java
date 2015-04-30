7afdbaece73b8bd0512941964d90814d04faf31f
gperry100 <gperry100@users.noreply.github.com>
2015-04-30 13:19:59 +0100

    Removing intellij commits

--- src/main/java/common/bss/com/bskyb/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://BSkyB.com.BSS.Common", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package common.bss.com.bskyb;
--- src/main/java/traffic/contract/bss/com/bskyb/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://BSkyB.com.BSS.Contract.Traffic", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package traffic.contract.bss.com.bskyb;
